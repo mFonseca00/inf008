01604cbf05b339ce43b73840e8fd2dacc52e18b5
Marcus <mv070900@gmail.com>
2025-05-08 14:03:32 -0300

    main ajustado e classe locadora completa

--- atividade_1/com/locadora/Main.java
+++ atividade_1/com/locadora/Main.java
@@ -1,17 +1,24 @@
 package atividade_1.com.locadora;
 
+import atividade_1.com.locadora.model.Locadora;
 import atividade_1.com.locadora.model.veiculos.Carro;
 import atividade_1.com.locadora.model.veiculos.Moto;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\n\nSistema de Locadora iniciado!");
+        Locadora locadora = new Locadora("Locadora XPTO");
 
-        Carro carro = new Carro("Fiat Uno", 2020, "KGA 2209", 4);
-        Moto moto = new Moto("Honda CG 160", 2024, "KLG 9932", 160);
+        Carro carro1 = new Carro("Civic", 2022, "ABC1234", 4);
+        Moto moto1 = new Moto("XRE 300", 2021, "XYZ9876", 300);
 
-        carro.exibirDetalhes();
-        moto.exibirDetalhes();
-        
+        locadora.cadastrarVeiculo(carro1);
+        locadora.cadastrarVeiculo(moto1);
+
+        locadora.listarVeiculos();
+
+        double valor = locadora.calcularValorTotalLocacao("ABC1234", 3);
+        if (valor >= 0) {
+            System.out.println("Valor da locação: R$ " + valor);
+        }
     }
 }
--- atividade_1/com/locadora/model/Locadora.java
+++ atividade_1/com/locadora/model/Locadora.java
@@ -1,5 +1,59 @@
 package atividade_1.com.locadora.model;
 
+import java.util.ArrayList;
+
+import atividade_1.com.locadora.model.veiculos.Veiculo;
+
 public class Locadora {
+    private String nome;
+    private ArrayList<Veiculo> veiculos;
+
+    public void cadastrarVeiculo(Veiculo v){
+        veiculos.add(v);
+    }
+
+    public void listarVeiculos(){
+        if (veiculos.isEmpty()) {
+            System.out.println("Nenhum veículo cadastrado.");
+            return;
+        }
+    
+        for (Veiculo v : veiculos) {
+            v.exibirDetalhes();
+        }
+    }
+
+    public double calcularValorTotalLocacao(String placa, int dias){
+        for(Veiculo v : veiculos){
+            if(v.getPlaca().equalsIgnoreCase(placa)){
+                double valor = v.calcularDiaria(dias);
+                return valor;
+            }
+        }
+        System.out.println("Veículo não cadastrado");
+        return -1;
+    }
+
+    public Locadora(String nome){
+        this.nome = nome;
+        this.veiculos = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public ArrayList<Veiculo> getVeiculos() {
+        return veiculos;
+    }
+
+    public void setVeiculos(ArrayList<Veiculo> veiculos) {
+        this.veiculos = veiculos;
+    }
+
     
 }
